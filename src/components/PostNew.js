3a4d34a27d9bfbd367008f61d4624fe1829e8ad4
David Henley <henleydr@gmail.com>
2017-05-07 11:38:56 -0500

    Redux Form

--- src/components/PostNew.js
+++ src/components/PostNew.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
-  render() {
+  onSubmit = (values) => {
+    console.log(values);
+  }
+
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+
     return (
-      <div className="PostNew container">
-        Post New
+      <div className="form-group">
+        <label className="form-control-label">{field.label}</label>
+        <input className="form-control form-control-danger" type="text" { ...field.input } />
+        <div className="text-danger">
+          {touched ? error : ''}
+        </div>
       </div>
+    );
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)} className="PostNew container">
+        <Field name="title" label="Title" component={this.renderField} />
+        <Field name="categories" label="Categories" component={this.renderField} />
+        <Field name="content" label="Post Content" component={this.renderField} />
+        <button type="submit" className="btn btn-primary"> Submit </button>
+      </form>
     );
   }
 }
 
-export default PostNew;
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.title) errors.title = 'Enter a title';
+  if (!values.categories) errors.categories = 'Enter at least one category';
+  if (!values.content) errors.content = 'Enter some post content';
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'newpost',
+  validate
+})(PostNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 
 import posts from './posts_reducer';
 
 export default combineReducers({
-  posts
+  posts, form
 });
